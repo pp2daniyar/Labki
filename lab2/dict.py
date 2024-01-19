5997f9cb77fcf29086cda140df3b22e9f490586d
admin <d_anetuly@kbtu.kz>
2024-01-20 01:10:50 +0500

    lab2

---
+++ lab2/dict.py
@@ -0,0 +1,10 @@
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print (car["model"])
+car["year"]=2020
+car["color"]="red"
+car.pop("model")
+car.clear()
---
+++ lab2/tuples.py
@@ -0,0 +1,6 @@
+fruits = ("apple", "banana", "cherry")
+print (fruits)
+print (len(fruits))
+print(fruits[-1])
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
