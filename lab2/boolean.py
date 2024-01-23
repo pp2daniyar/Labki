f38d991803bd62e1dadb1ef2851f47afe99b91aa
admin <d_anetuly@kbtu.kz>
2024-01-23 14:37:30 +0500

    comments

--- lab2/boolean.py
+++ lab2/boolean.py
@@ -1,5 +1,14 @@
+#Exercise 1
 print (10>9)
+
+#Exercise 2
 print(10 == 9)
+
+#Exercise 3
 print(10 < 9)
+
+#Exercise 4
 print(bool("abc"))
+
+#Exercise 5
 print(bool(0))
--- lab2/ifelse.py
+++ lab2/ifelse.py
@@ -1,30 +1,47 @@
+#Exercise 1
 a=50
 b=10
 c=5
 d=5
 if (a>b):
     print("Hello World")
+
+#Exercise 2
 if a!=b:
     print("Hello World")
+
+#Exercise 3
 if (a==b):
     print("Yes")
 else:
     print("No")
+
+#Exercise 4
 if (a==b):
     print (1)
 elif(a>b):
     print(2)
 else:
     print (3)
+
+#Exercise 5
 if a==b and c==d:
     print("Hello")
+
+#Exercise 6
 if a==b or c==d:
     print("Hello")
-if 5>2:
+
+#Exercise 7
+if 5>2: 
     print("YES")
+
+#Exercise 8
 a=2
 b=5
 print("YES") if a==b else print("NO")
+
+#Exercise 9
 a = 2
 b = 50
 c = 2
